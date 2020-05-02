3703be33b56ee6c1f5012137bc9fffb21af9e794
rs.rupesh95 <rs.rupesh95@gmail.com>
2020-05-02 23:20:46 +0530

    trying out changes

--- index.js
+++ index.js
@@ -1,12 +1,17 @@
-import "@babel/polyfill";
+import "@babel/polyfill/noConflict";
 import { GraphQLServer } from "graphql-yoga";
 import Query from "./resolvers/Query";
 import mongoose from "mongoose";
 import Mutation from "./resolvers/Mutation";
 const port = process.env.PORT || 4000;
-mongoose.connect('mongodb://admin:pwd123@ds127115.mlab.com:27115/localize', {useNewUrlParser: true, useUnifiedTopology: true}).then(()=>{
-  console.log("mongodb connected");
-});
+mongoose
+    .connect("mongodb://admin:pwd123@ds127115.mlab.com:27115/localize", {
+        useNewUrlParser: true,
+        useUnifiedTopology: true
+    })
+    .then(() => {
+        console.log("mongodb connected");
+    });
 
 const server = new GraphQLServer({
     typeDefs: "./schema.graphql",
@@ -15,4 +20,15 @@ const server = new GraphQLServer({
         Mutation
     }
 });
-server.start({ port } , () => console.log(`Server is running on http://localhost:4000`));
+server.start(
+    {
+        port,
+        cors: {
+            origin: "*",
+            methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+            preflightContinue: false,
+            optionsSuccessStatus: 204
+        }
+    },
+    () => console.log(`Server is running on http://localhost:4000`)
+);
