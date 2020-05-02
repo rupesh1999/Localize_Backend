4ef9ab6abffb6401749638f13822b9aac82757bd
rs.rupesh95 <rs.rupesh95@gmail.com>
2020-05-02 21:38:57 +0530

    production deployment setup

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
-// const { GraphQLServer } = require("graphql-yoga");
+import "@babel/polyfill";
 import { GraphQLServer } from "graphql-yoga";
 import Query from "./resolvers/Query";
 import mongoose from "mongoose";
 import Mutation from "./resolvers/Mutation";
- 
+const port = process.env.PORT || 4000;
 mongoose.connect('mongodb://admin:pwd123@ds127115.mlab.com:27115/localize', {useNewUrlParser: true, useUnifiedTopology: true}).then(()=>{
   console.log("mongodb connected");
 });
@@ -15,4 +15,4 @@ const server = new GraphQLServer({
         Mutation
     }
 });
-server.start(() => console.log(`Server is running on http://localhost:4000`));
+server.start({ port } , () => console.log(`Server is running on http://localhost:4000`));
